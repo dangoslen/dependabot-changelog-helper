2e72a35e35e012d256c4115d174f7d3f7098b8f5
Dan Goslen <dgoslen@bandwidth.com>
2021-03-04 09:24:26 -0500

    Fixing regex

--- __tests__/entry-extractor.test.ts
+++ __tests__/entry-extractor.test.ts
@@ -7,6 +7,13 @@ const PULL_REQUEST_EVENT = {
   }
 }
 
+const PULL_REQUEST_EVENT_ODD_PACKAGE = {
+  pull_request: {
+    number: 123,
+    title: 'Bumps @package-with_odd_characters+ from 6.0 to 7.0'
+  }
+}
+
 const PULL_REQUEST_EVENT_ALPHA_TO_BETA = {
   pull_request: {
     number: 123,
@@ -30,4 +37,14 @@ test('extracts package and -alpha -beta versions', async () => {
   expect(entry.package).toStrictEqual('package-with-dashes')
   expect(entry.oldVersion).toStrictEqual('6.0-alpha')
   expect(entry.newVersion).toStrictEqual('6.0-beta')
+})
+
+test('extracts package with odd package name', async () => {
+  const entry: DependabotEntry = getDependabotEntry(
+    PULL_REQUEST_EVENT_ODD_PACKAGE
+  )
+
+  expect(entry.package).toStrictEqual('@package-with_odd_characters+')
+  expect(entry.oldVersion).toStrictEqual('6.0')
+  expect(entry.newVersion).toStrictEqual('7.0')
 })
