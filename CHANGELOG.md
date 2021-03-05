55427e458f6f52d0b523a52289067b4a5d7413ec
Dan Goslen <dwgoslen@gmail.com>
2021-03-05 15:41:31 -0500

    Stubbing out dependabot helper core logic; meta-data about package (#6)

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Changelog
+
+## [0.1.0]
+### Added 
+- Adds initial logic for updating a changelog based on the [KeepAChangelog](https://keepachangelog.com/en/1.0.0/) format
+### Dependencies
+- Bumps `eslint` from 7.17.0 to 7.21.0
+- Bumps `@types/node` from 14.14.30 to 14.14.31
+- Bumps `eslint-plugin-jest` from 24.1.3 to 24.1.5
---
+++ src/entry-extractor.ts
@@ -0,0 +1,32 @@
+import {WebhookPayload} from '@actions/github/lib/interfaces'
+
+/** Regex explanation
+ *                                 --- Matches Bump or Bumps
+ *                                 |     --- Matches any non-whitespace character; matching as a few as possible
+ *                                 |     |          --- Matches any non-whitespace character
+ *                                 |     |          |          --- Matches any non-whitespace character
+ *                                 |     |          |          |
+ */
+const TITLE_REGEX = new RegExp(/Bumps? (\S+?) from (\S*) to (\S*)/)
+
+export interface DependabotEntry {
+  pullRequestNumber: number
+  package: string
+  oldVersion: string
+  newVersion: string
+}
+
+export function getDependabotEntry(event: WebhookPayload): DependabotEntry {
+  const pullRequestNumber: number = event.pull_request!.number
+  const titleResult = TITLE_REGEX.exec(event.pull_request!.title)
+  if (titleResult === null) {
+    throw new Error('Unable to extract entry from pull request title!')
+  }
+
+  return {
+    pullRequestNumber,
+    package: titleResult[1],
+    oldVersion: titleResult[2],
+    newVersion: titleResult[3]
+  }
+}
