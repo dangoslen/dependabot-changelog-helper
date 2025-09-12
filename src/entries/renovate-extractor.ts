a2b6383e82d3384281eadd8ea2f1c5bc07def0e2
Valentin Brandl <mail@vbrandl.net>
2025-09-12 16:58:17 +0200

    improve change extraction for renovate

--- src/entries/renovate-extractor.ts
+++ src/entries/renovate-extractor.ts
@@ -16,37 +16,49 @@ export class RenovateExtractor implements EntryExtractor {
       return entries
     }
 
-    // Renovate uses a table format in its PR description
-    // Example:
+    // Renovate uses different table formats in its PR description
+    // Examples:
     // | Package | Changes |
     // | --- | --- | --- |
     // | [package-name](link.com) | [1.0.0 -> 2.0.0](link.com) |
+    //
+    // | Package | Type | Update | Change |
+    // | --- | --- | --- | --- |
+    // | [ghcr.io/renovatebot/renovate](https://renovatebot.com) ([source](https://github.com/renovatebot/renovate)) | container | minor | `41.100.0` -> `41.109.0` |
+
     const lines = body.split('\n')
     let inDependencyTable = false
+    let packageColumn = -1
+    let changeColumn = -1
     for (const line of lines) {
-      if (line.startsWith('| Package | Changes |')) {
-        inDependencyTable = true
+      if (line.includes('| Package |')) {
+        const columns = line
+          .trim()
+          .split('|')
+          .map(s => s.trim())
+        packageColumn = columns.indexOf('Package')
+        changeColumn = columns.findIndex(v => v === 'Change' || v === 'Changes')
+
+        inDependencyTable = packageColumn !== -1 && changeColumn !== -1
         continue
       }
 
       if (inDependencyTable && line.startsWith('|')) {
-        const [_, pkg, changes, __] = line.split('|').map(s => s.trim())
+        const columns = line.split('|').map(s => s.trim())
+        const pkg = columns[packageColumn]
+        const changes = columns[changeColumn]
 
         if (pkg && changes) {
-          const pkgName = this.linkRegex.exec(pkg)?.[1]
-          const linkText = this.linkRegex.exec(changes)?.[1]
+          const pkgName = this.extractPkgName(pkg)
+          const versions = this.extractVersion(changes)
 
-          const [oldVersion, newVersion] = (linkText?.split('->') ?? []).map(
-            v => v.trim()
-          )
-
-          if (pkgName && oldVersion && newVersion) {
+          if (pkgName && versions) {
             entries.push({
               pullRequestNumber: event.pull_request?.number || 0,
               repository: undefined,
               package: pkgName,
-              oldVersion,
-              newVersion
+              oldVersion: versions.oldVersion,
+              newVersion: versions.newVersion
             })
           }
         }
@@ -55,5 +67,25 @@ export class RenovateExtractor implements EntryExtractor {
       }
     }
     return entries
+  }
+
+  private extractPkgName(columnValue: string): string | undefined {
+    return this.linkRegex.exec(columnValue)?.[1]
+  }
+
+  private extractVersion(
+    columnValue: string
+  ): {oldVersion: string; newVersion: string} | undefined {
+    // the version change can be either in plaintext or as a markdown link
+    // try to extract the value if it looks like a markdown link, else assume plaintext
+    const versions = this.linkRegex.exec(columnValue)?.[1] ?? columnValue
+
+    const [oldVersion, newVersion] = versions
+      // the plaintext variant formats each version as `1.0.0` in backticks
+      .replaceAll('`', '')
+      .split('->')
+      .map(v => v.trim())
+
+    return oldVersion && newVersion ? {oldVersion, newVersion} : undefined
   }
 }
